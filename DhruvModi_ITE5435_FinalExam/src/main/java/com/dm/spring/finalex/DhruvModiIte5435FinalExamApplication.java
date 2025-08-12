11f061b6ce3409df8ca62385caea25dc676ca4de
Dhruvmodiii <Dhruvmodi240400@gmail.com>
2025-08-12 01:49:02 -0400

    Final Exam

---
+++ DhruvModi_ITE5435_FinalExam/src/main/java/com/dm/spring/finalex/DhruvModiIte5435FinalExamApplication.java
@@ -0,0 +1,13 @@
+package com.dm.spring.finalex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhruvModiIte5435FinalExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhruvModiIte5435FinalExamApplication.class, args);
+	}
+
+}
---
+++ DhruvModi_ITE5435_FinalExam/src/test/java/com/dm/spring/finalex/DhruvModiIte5435FinalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dm.spring.finalex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhruvModiIte5435FinalExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
